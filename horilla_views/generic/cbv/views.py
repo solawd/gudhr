4ea78f0c23e1ad053e2c17dc21c9b303670d602d
Solomon Apenya <solawd@gmail.com>
2025-12-16 11:32:39 +0000

    Change Horilla name on view

--- horilla_views/generic/cbv/views.py
+++ horilla_views/generic/cbv/views.py
@@ -1464,8 +1464,8 @@ class HorillaDetailedView(DetailView):
     title = "Detailed View"
     template_name = "generic/horilla_detailed_view.html"
     header: dict = {
-        "title": "Horilla",
-        "subtitle": "Horilla Detailed View",
+        "title": "GudHR",
+        "subtitle": "GudHR Detailed View",
         "avatar": "",
     }
     body: list = []
